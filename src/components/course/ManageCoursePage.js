54cb7dbadbb1ec2f5690866d6609e09c679a72bb
Himanshu Dhomse <hadhomse@cygnet.com>
2017-05-30 17:02:49 +0530

    Complete Course Page with error handling

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import toastr from 'toastr';
 
 export class ManageCoursePage extends React.Component {
   constructor(props, context) {
@@ -10,7 +11,8 @@ export class ManageCoursePage extends React.Component {
 
     this.state = {
       course: Object.assign({}, props.course),
-      errors: {}
+      errors: {},
+      saving: false
     };
 
     this.updateCourseState = this.updateCourseState.bind(this);
@@ -24,9 +26,21 @@ export class ManageCoursePage extends React.Component {
     }
   saveCourse(event) {
    event.preventDefault();
-   this.props.actions.saveCourse(this.state.course);
-   this.context.router.push('/courses');
+   this.setState({saving:true});
+   this.props.actions.saveCourse(this.state.course)
+   .then(()=> this.redirect())
+   .catch(error=>{
+        this.setState({saving:false});
+     toastr.error(error);
+   });
  }
+
+redirect(){
+  this.setState({saving:false});
+  toastr.success('Course Saved');
+  this.context.router.push('/courses');
+}
+
   updateCourseState(event) {
    const field = event.target.name;
    let course = this.state.course;
@@ -42,6 +56,7 @@ export class ManageCoursePage extends React.Component {
         onSave={this.saveCourse}
         course={this.state.course}
         errors={this.state.errors}
+        saving={this.state.saving}
       />
     );
   }
--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -9,7 +9,8 @@ export default function ajaxStatusReducer(state = initialState.ajaxCallsInProgre
   if(action.type == types.BEGIN_AJAX_CALL){
     return state + 1;
   }
-  else if (actionTypesEndsInSuccess(action.type)) {
+  else if (action.type == types.AJAX_CALL_ERROR ||
+    actionTypesEndsInSuccess(action.type)) {
     return state - 1;
   }
   return state;
